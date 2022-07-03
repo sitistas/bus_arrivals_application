b2c90b5baf521ed68c81ffd10b9a147d31d94e91
sitistas <kon.sitistas@gmail.com>
2022-07-04 00:40:16 +0300

    Fix id & line

--- test-requests.py
+++ test-requests.py
@@ -11,18 +11,16 @@ load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
 # Το id παραμένει ίδιο στο πρώτο request και στο κατέβασμα του zip, οπότε μπορούμε να το περνάμε σαν όρισμα
-id = input('Βάλε το Data ID: ')  # or 464
-if id == '':
-    id = '464'
+id = int(input('Βάλε το Data ID: ') or 464)  # or 464
 # Όνομα γραμμής
 line_name = input('Βάλε το όνομα της γραμμής: ')  # or 'SP'
 if line_name == '':
     line_name = 'SP'
 # Όνομα μέρας
 day = input('Διάλεξε μέρα: ')
-if day not in ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday','Today']:
-    day='Monday'
-if day=='Today':
+if day not in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Today']:
+    day = 'Monday'
+if day == 'Today':
     today = datetime.datetime.now()
     day = today.strftime('%A')
 print(day)
@@ -32,11 +30,11 @@ print(day)
 
 
 def return_line_data(id, line_name):
-    df = pandas.read_csv('overall_data_sample.csv')
-    mask = (df['Line Name'] == line_name)  # & (df['Data ID'] == id)
-    # ΔΕ ΛΕΙΤΟΥΡΓΕΙ ΣΩΣΤΑ, ΠΡΕΠΕΙ ΝΑ ΜΠΑΙΝΕΙ ΣΑΝ ΦΙΛΤΡΟ ΚΑΙ ΤΟ ID
-    df_new = df[mask]
-    print(df_new)
+    df = pandas.read_csv('overall_data_catalogue.csv')
+    df_new1 = df[df['Line Name'] == line_name]
+    # print(df_new1)
+    df_new = df_new1[df_new1['Data ID'] == id]
+    # print(df_new)
     file_name = df_new['TXC File Name'].iloc[0]
     print(df_new[['Operator', 'Line Name']])
     print(file_name)
@@ -65,8 +63,8 @@ files = archive.namelist()
 
 # print(files)
 
-# file_name = return_line_data(id, line_name) TO BE FIXED
-file_name = 'BODS_PF0007157_12_20220129_0.xml'
+file_name = return_line_data(id, line_name)  # TO BE FIXED
+# file_name = 'BODS_PF0007157_12_20220129_0.xml'
 
 with archive.open(file_name, "r") as fi:
     data = fi.read()
@@ -78,21 +76,23 @@ xml_fname = archive.extract(file_name)
 # or xml.dom.minidom.parseString(xml_string)
 dom = xml.dom.minidom.parse(xml_fname)
 
-#Get the departure's list for a particular line and day
+# Get the departure's list for a particular line and day
+
+
 def getDeps(day, line_name):
-# Get all Vehicle Journeys
-    depList=[]
+    # Get all Vehicle Journeys
+    depList = []
     vehiclejs = dom.getElementsByTagName('VehicleJourney')
 
     # Δες ποια δρομολόγια εκτελούνται τη μέρα που θες (πχ Δευτέρα)
     for elem in vehiclejs:
-        lineRef=elem.getElementsByTagName('LineRef')
-        #Οι τελευταίοι χαρακτήρες (1 ή 2) του lineRef ενός VehicleJourney αντιστοιχούν στον αριθμό της γραμμής
-        lineName=(lineRef[0].firstChild.data).rsplit(':',1)
-        #Αν η γραμμή του VehicleJourney αντιστοιχεί σε άλλη γραμμή από αυτή που θέλουμε
-        if lineName[1]!=line_name:
+        lineRef = elem.getElementsByTagName('LineRef')
+        # Οι τελευταίοι χαρακτήρες (1 ή 2) του lineRef ενός VehicleJourney αντιστοιχούν στον αριθμό της γραμμής
+        lineName = (lineRef[0].firstChild.data).rsplit(':', 1)
+        # Αν η γραμμή του VehicleJourney αντιστοιχεί σε άλλη γραμμή από αυτή που θέλουμε
+        if lineName[1] != line_name:
             continue
-    
+
         days = elem.getElementsByTagName('DaysOfWeek')
         x = days[0].getElementsByTagName(day)
         # Αν για ένα δρομολόγιο υπάρχει η σχετικέ μέρα στις μέρες εκτέλεσης, τύπωσε την ώρα εκκίνησης
@@ -101,7 +101,8 @@ def getDeps(day, line_name):
             depList.append(depttime[0].firstChild.data)
     return depList
 
-departureList=getDeps(day, line_name)
+
+departureList = getDeps(day, line_name)
 print(departureList)
 # pretty_xml_as_string = dom.toprettyxml()
 # print(pretty_xml_as_string)
